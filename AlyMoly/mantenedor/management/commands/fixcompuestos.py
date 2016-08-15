761f15f97350a46c2935817b72893c96c8c10731
Andrés Otárola <andres@otarola.me>
2016-08-14 21:22:37 -0700

    Fixed commandline script

--- AlyMoly/mantenedor/management/commands/fixcompuestos.py
+++ AlyMoly/mantenedor/management/commands/fixcompuestos.py
@@ -10,25 +10,17 @@
 # 2010                                        #
 ###############################################
 
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
-from mantenedor.models import Producto
+from AlyMoly.mantenedor.models import Producto
 from django.db import connection
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = u"Corrige el atributo es_compuesto productos compuestos correspondientes."
-    
-    @transaction.commit_manually
-    def handle_noargs(self, **options):
-        try:
-            #Se obtienen los datos que se encuentren sin sincronizar
-            compuestos_id = Producto.objects.exclude(compone=None).values_list('compone',flat=True)
-            compuestos = Producto.objects.filter(id__in=compuestos_id)
-            compuestos.update(es_compuesto=True)
-            transaction.commit()
-        except:
-            transaction.rollback()
-            raise #levanto la última excepción
-        finally:
-            connection.close()
-            
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        #Se obtienen los datos que se encuentren sin sincronizar
+        compuestos_id = Producto.objects.exclude(compone=None).values_list('compone',flat=True)
+        compuestos = Producto.objects.filter(id__in=compuestos_id)
+        compuestos.update(es_compuesto=True)
