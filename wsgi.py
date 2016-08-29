d4db998ebbd4a5c910aa68eead323724fea31322
Andrés Otárola <andres@otarola.me>
2016-08-28 17:49:04 -0700

    Fixed wsgi location

--- wsgi.py
+++ wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alymoly.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AlyMoly.settings')
 
 application = get_wsgi_application()
